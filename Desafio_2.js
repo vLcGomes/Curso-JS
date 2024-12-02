866d3715cd36443c4c639393c6bb466ca214525f
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-12-02 12:45:55 -0300

    Primeiros Passos em Java Script

---
+++ Desafio_2.js
@@ -0,0 +1,7 @@
+// PI * (raio * raio)
+
+const PI = 3.141592
+let raio = 10;
+let circunferencia = PI * (raio * raio)
+
+console.log('A área é de ' + circunferencia + 'm2')
---
+++ Desafio_3.js
@@ -0,0 +1,10 @@
+let a = 7
+let b = 94
+
+const valorInicialA = a
+
+a = b
+b = valorInicialA
+
+console.log(a)
+console.log(b)
