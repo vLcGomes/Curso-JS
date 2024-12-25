eeaef4572f4890c55cd968fc40f5a0cd725ee52e
Vinicius Gomes <165200823+vLcGomes@users.noreply.github.com>
2024-12-25 20:34:17 -0300

    Update funcionarios.js

--- JSGeral/Node/funcionarios/funcionarios.js
+++ JSGeral/Node/funcionarios/funcionarios.js
@@ -1,6 +1,22 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+// const chineses = f => f.pais === 'China'
+// const mulheres = f => f.genero === 'F'
+// const menorSalario = (f, fAtual) => {
+//   return f.salario < fAtual.salario ? f : fAtual
+// }
+
+// axios.get(url).then(response => {
+//   const funcionarios = response.data
+//   const funcionariaComMenorSalario = funcionarios.filter(chineses).filter(mulheres).reduce(menorSalario)
+
+//   console.log(funcionariaComMenorSalario)
+// })
+
+
+
+
 axios.get(url).then(response => {
   const funcionarios = response.data
   const funcionariasChinesas = funcionarios.filter(funcionario => funcionario.pais == 'China' && funcionario.genero == 'F')
