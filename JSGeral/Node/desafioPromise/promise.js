6ced63ab95768bb6457ae05e21c970c7d8dfb38f
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-12-30 14:05:45 -0300

    Atualização de arquivo

--- JSGeral/Node/desafioPromise/promise.js
+++ JSGeral/Node/desafioPromise/promise.js
@@ -4,12 +4,6 @@ const path = require('path')
 const caminho = path.join(__dirname, 'dados.txt')
 
 
-
-// const lerArquivo = new Promise(resolve => {
-//   resolve(fs.readFile(caminho, ImprimeTexto))
-// })
-
-
 function lerArquivo(caminho) {
   return new Promise(resolve => fs.readFile(caminho, function(_, conteudo) {
     resolve(conteudo.toString())
